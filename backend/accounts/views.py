53afe8422831013461118150cb82e7e24611dc7c
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-01-28 04:49:35 +0300

    Completed: Email authentication + verification

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from django.http import HttpResponseRedirect
+from django.conf import settings 
+import requests
 
-# Create your views here.
+class ActivationView(APIView):
+    permission_classes = []
+
+    def get(self, request, uid, token):
+        activation_url = request.build_absolute_uri('/auth/users/activation/')
+        payload = {'uid': uid, 'token': token}
+
+        resp = requests.post(activation_url, json=payload)
+
+        if resp.status_code in (200, 204):
+            return HttpResponseRedirect(f"{settings.BASE_URL}/auth/jwt/create/")
+        else:
+            return HttpResponseRedirect(f"{settings.BASE_URL}/auth/users/")

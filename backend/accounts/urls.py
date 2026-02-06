4d39267f0f17b5a36bb85de75faaaec8a98de848
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-02-06 18:22:21 +0300

    created profile model and api for current user

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import AuthViewSet
+from .views import AuthViewSet, ProfileViewSet
 
 router = DefaultRouter()
+router.register('profile', ProfileViewSet, basename='profile')
 router.register('', AuthViewSet, basename='auth-actions')
 
 urlpatterns = router.urls

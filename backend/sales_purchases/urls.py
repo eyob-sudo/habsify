09aa9b793110ec241f7d2547d0ae224102e3ae72
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-18 09:24:04 +0300

    fix: add new validator into task apps and add new dropdown rout

--- backend/sales_purchases/urls.py
+++ backend/sales_purchases/urls.py
@@ -1,10 +1,23 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import SaleViewSet, PurchaseViewSet,PurchaseDropdownViewSet,SaleDropdownViewSet
+from .views import (
+    SaleViewSet, 
+    PurchaseViewSet,
+    PurchaseDropdownViewSet,
+    SaleDropdownViewSet,
+    SupplierListForDropdown,
+    ItemListForDropdown,
+    WarehouseListForDropdown
+)
 
 router = DefaultRouter()
-router.register('sales', SaleViewSet,basename='sales')
+router.register('sales', SaleViewSet, basename='sales')
 router.register('purchases', PurchaseViewSet, basename='purchase')
-router.register("purchase-dropdown",PurchaseDropdownViewSet,basename="purchase-dropdown")
-router.register("sale-dropdown",SaleDropdownViewSet,basename="sale-dropdown")
+router.register('purchase-dropdown', PurchaseDropdownViewSet, basename='purchase-dropdown')
+router.register('sale-dropdown', SaleDropdownViewSet, basename='sale-dropdown')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('suppliers/dropdown/', SupplierListForDropdown.as_view(), name='supplier-dropdown'),
+    path('items/dropdown/', ItemListForDropdown.as_view(), name='item-dropdown'),
+    path('warehouses/dropdown/', WarehouseListForDropdown.as_view(), name='warehouse-dropdown'),
+]

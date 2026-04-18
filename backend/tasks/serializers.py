58dfcaeda3d33944fe69537771bf84e17e331793
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-18 09:24:12 +0300

    fix: add new validator into task apps and add new dropdown rout

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.utils import timezone
+from django.db.models.query import QuerySet
 from .models import Task
+
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,13 +13,51 @@ class TaskSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['completed_at', 'created_at', 'created_by']
 
-    def validate_due_date(self, value):
-        if value < timezone.now().date():
-            raise serializers.ValidationError("Due date cannot be in the past.")
-        return value
+    def get_extra_kwargs(self):
+        extra_kwargs = super().get_extra_kwargs()
+
+        if (
+            self.instance
+            and not isinstance(self.instance, QuerySet)
+            and getattr(self.instance, 'completed', False)
+        ):
+            for field in ['title', 'priority', 'due_date']:
+                extra_kwargs.setdefault(field, {})
+                extra_kwargs[field]['read_only'] = True
+
+        return extra_kwargs
+
+    def validate(self, data):
+        if self.instance and self.instance.completed:
+
+            forbidden_fields = ['title', 'priority', 'due_date']
+            for field in forbidden_fields:
+                if field in self.initial_data:         
+                    raise serializers.ValidationError({
+                        "detail": "Completed tasks cannot be edited."
+                    })
+
+            if self.initial_data.get('completed') is False:
+                raise serializers.ValidationError({
+                    "completed": "You cannot undo a completed task. Once finished, it stays completed forever."
+                })
+
+        due_date = data.get('due_date')
+        if due_date and due_date < timezone.now().date():
+            raise serializers.ValidationError({
+                "due_date": "Due date cannot be in the past."
+            })
+
+        return data
+
+    def update(self, instance, validated_data):
+        if validated_data.get('completed') is True and not instance.completed:
+            validated_data['completed_at'] = timezone.now()
+
+        return super().update(instance, validated_data)
 
     def create(self, validated_data):
-        validated_data.pop("completed", None)
+        validated_data.pop("completed", None)   
         validated_data['created_by'] = self.context['request'].user
         validated_data['company'] = self.context['request'].user.company
         return super().create(validated_data)

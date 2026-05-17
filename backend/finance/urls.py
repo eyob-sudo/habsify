06acffe02ec546e67e4bb068f988472c94b374c6
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-17 14:36:22 +0300

    style: format import statements for better readability and add transaction type dropdown route

--- backend/finance/urls.py
+++ backend/finance/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import AccountViewSet,TransactionViewSet,finance_stats, cash_management,ExpenseCreateView
+from .views import (AccountViewSet,
+                    TransactionViewSet,
+                    finance_stats, 
+                    cash_management,
+                    ExpenseCreateView,
+                    transaction_type_dropdown)
 
 routes = DefaultRouter()
 routes.register('accounts', AccountViewSet, basename='account')
@@ -10,5 +15,6 @@ urlpatterns = routes.urls + [
     path('stats/', finance_stats, name='stats-management'),
     path('cash/', cash_management, name='cash-management'),
     path('expenses/', ExpenseCreateView.as_view(), name='expense-create'),
-
+    path('transaction-types/', transaction_type_dropdown, name='transaction-type-dropdown'),
 ]
+

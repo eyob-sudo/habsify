75f40488f4d92b3ecaba800aaab7097c823c4bb4
eyob-sudo <eyob.english@gmail.com>
2026-06-29 22:55:16 +0300

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './authService'
 import { attachGlobalErrorHandler } from './globalErrorHandler'
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || "https://habsify-api.up.railway.app";
 
 
 const api = axios.create({
